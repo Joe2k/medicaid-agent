fe967f92fa4e12030bbda3bc810c1f330246b5d8
Jonathan Samuel <jonathansamuel2k@gmail.com>
2025-11-10 19:39:15 -0800

    Add Medicaid UI application

---
+++ src/main/java/org/usfca/medicaid/MedicaidUIApplication.java
@@ -0,0 +1,13 @@
+package org.usfca.medicaid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicaidUIApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedicaidUIApplication.class, args);
+    }
+}
+
